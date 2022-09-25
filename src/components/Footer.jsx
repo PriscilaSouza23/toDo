d47de39ee7dc779d8b23ed1da0610bf5d76f725e
PriscilaLSouza <priscila.souza@cmosdrake.com.br>
2022-09-25 18:35:25 -0300

    finish project toDo

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className="attribution">
+          <p>
+            Challenge by{' '}
+            <a href="">Frontend Mentor</a>. Coded
+            by{' '}
+            <a href="" title="">
+              
+            </a>
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+};
